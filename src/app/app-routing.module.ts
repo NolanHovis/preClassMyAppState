cca0f4f681db69a7dd37fffcbfee25d72007e04c
Nolan Hovis <78943909+NolanHovis@users.noreply.github.com>
2021-07-01 18:58:34 -0500

    Animations & Offline Service Workers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
+import { animate } from '@angular/animations';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AnimateComponent } from './animate/animate.component';
 import { HomeComponent } from './home/home.component';
 import { JustCurrentuserValueComponent } from './just-currentuser-value/just-currentuser-value.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home'},
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'just-currentuser-value', component: JustCurrentuserValueComponent },
   {
     path: 'auth', loadChildren: () => import('./auth/auth.module')
       .then(mod => mod.AuthModule)
   },
+  { path: 'animate', component: AnimateComponent},
   { path: '**', component: NotFoundComponent }
 ];
 
